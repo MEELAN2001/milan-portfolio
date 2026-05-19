5e09d655e4cc86517ed05109f2eb6cf227078a38
MEELAN2001 <2001milantamang@gmail.com>
2026-05-19 15:49:58 +0545

    Add sitemap function to define site URLs and last modified dates for SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://milanbomjantamang.com.np",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milanbomjantamang.com.np/#about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milanbomjantamang.com.np/#projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milanbomjantamang.com.np/#contact",
+      lastModified: new Date(),
+    },
+  ];
+}
